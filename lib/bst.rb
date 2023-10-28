b2b6a7b720185163e12232eee639c27695efd546
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-28 23:30:57 +0200

    Implementing the #insert

--- lib/bst.rb
+++ lib/bst.rb
@@ -7,4 +7,16 @@ class Tree
     @root = Node.new
   end
 
+  def insert(value)
+    current_node = @root
+    if current_node.value.nil? 
+      current_node.value = value
+    else 
+      until current_node.left.nil? || current_node.right.nil?
+        current_node = current_node.left until current_node.left.nil? || current_node.value < value
+        current_node = current_node.right until current_node.right.nil? || current_node.value >= value
+      end
+    end
+
+  end
 end
--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,9 @@
 class Node 
   attr_accessor :value, :left, :right
-  
-  def initialize(value = nil, left = nil, right = nil)
-     @value = value
-     @left = left
-     @right = right
-  end
 
+  def initialize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
 end
