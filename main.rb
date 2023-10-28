d5da75c6469fc76047186091439c4ae90b496bf4
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-28 23:53:11 +0200

    Still implementing #insert

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require_relative 'lib/node'
 require_relative 'lib/bst'
 
-def pretty_print(node = @root, prefix = '', is_left = true)
-  pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-  pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-end
+# def pretty_print(node = @root, prefix = '', is_left: false)
+#   pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+#   puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+#   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+# end
 
 bst = Tree.new
 
@@ -13,4 +13,5 @@ bst = Tree.new
 
 (10..20).reverse_each { |k| bst.insert(k) }
 
-pretty_print(bst)
+p bst
+# pretty_print(bst)
