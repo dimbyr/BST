ab7bc751335c85e91447eefffb185a9de57ca7b6
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-31 22:55:36 +0200

    Created balanced BST

--- lib/tree.rb
+++ lib/tree.rb
@@ -22,7 +22,7 @@ class Tree
     root
   end
 
-  def pretty_print(node = @root, prefix = '', is_left: true)
+  def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
