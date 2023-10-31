de07ad3b7a1f0946046c83794db844fe46e28a7d
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-31 23:09:26 +0200

    added #insert method

--- lib/tree.rb
+++ lib/tree.rb
@@ -22,6 +22,20 @@ class Tree
     root
   end
 
+  def insert(value, node = @root)
+    if node.data.nil?
+      node.data = value
+    elsif node.left.nil? && value < node.data 
+      node.left = Node.new(value)
+    elsif node.right.nil? && value >= node.data
+      node.right = Node.new(value)
+    elsif value < node.data 
+      insert(value, node.left)
+    elsif value >= node.data
+      insert(value, node.right)
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
