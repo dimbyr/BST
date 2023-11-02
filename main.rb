474009873ebeb35711070f75a83b8ac846490f9d
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-11-02 12:39:58 +0200

    commented out all related to 'bst.rb'

--- main.rb
+++ main.rb
@@ -1,14 +1,15 @@
 require_relative 'lib/node'
-require_relative 'lib/bst'
+# require_relative 'lib/bst'
 require_relative 'lib/tree'
 
-bst = BinaryTree.new
+# bst = BinaryTree.new
 
-20.times do 
-  k = rand(200)
-  bst.insert(k) 
-end
-sep = '='*40
+# 20.times do 
+#   k = rand(200)
+#   bst.insert(k) 
+# end
+
+sep = '=' * 40
 ls = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 33, 23, 13, 11, 12]
 tree = Tree.new(ls)
 tree.pretty_print
@@ -46,5 +47,3 @@ puts sep
 puts 'searching for 8'
 puts tree.find(8)
 puts sep
-# bst.pretty_print()
-# pretty_print(bst)
